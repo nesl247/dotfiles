384accecabd4a0dbd7fdaba41e23103023388a24
Harrison Heck <harrison@theheck.me>
2014-12-19 10:24:17 -0500

    Created installer file. Moved things into directories: confs, resources

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+for x in confs/*;
+do
+	file=$(basename $x)
+	source=conf/${file}
+	target=${HOME}/.${file}
+	
+	if ! test -L $target; then
+		ln -s ${source} ${target}
+	elif test -f $target; then
+		echo "${target} already exists. It must be removed and then re-run this installer"
+	fi
+done
