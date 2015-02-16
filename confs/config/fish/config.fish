39ce7d7792bffa1fddb4971d81a8b2326e72ca66
Harrison Heck <harrison@theheck.me>
2015-02-16 17:51:28 -0500

    Add config.zsh. Disable autostarting TMUX

---
+++ confs/config/fish/config.fish
@@ -0,0 +1,35 @@
+# Path to your oh-my-fish.
+set fish_path $HOME/.oh-my-fish
+
+# Theme
+#set fish_theme robbyrussell
+set fish_theme bobthefish
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+# Example format: set fish_plugins autojump bundler
+
+# Path to your custom folder (default path is $FISH/custom)
+#set fish_custom $HOME/dotfiles/oh-my-fish
+
+# Load plugins
+set fish_plugins git tab
+
+# Theme Settings
+set theme_display_ruby no
+set theme_display_virtualenv no
+set theme_display_user yes
+set default_user harrison
+
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
+
+# Custom Path
+set fish_user_paths $fish_user_paths ~/Code/scripts
+set fish_user_paths $fish_user_paths ~/.composer/vendor/bin
+if test -e (which brew)
+	set fish_user_paths $fish_user_paths (brew --prefix homebrew/php/php56)/bin
+end
+
+# Aliases
+alias git "hub"
