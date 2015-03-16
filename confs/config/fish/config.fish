ce541d7fe70dbc0d7c27240a90f7408848affb55
Harrison Heck <harrison.heck@gmail.com>
2015-03-16 12:12:07 -0400

    Improved fish startup speed by reducing the set universal call to 1.

--- confs/config/fish/config.fish
+++ confs/config/fish/config.fish
@@ -12,9 +12,6 @@ set fish_theme bobthefish
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
 
-# Load plugins
-set fish_plugins git tab
-
 # Theme Settings
 set theme_display_ruby no
 set theme_display_virtualenv no
@@ -25,17 +22,22 @@ set default_user harrison
 . $fish_path/oh-my-fish.fish
 
 # Custom Path
-set -U fish_user_paths /usr/local/opt/coreutils/libexec/gnubin $fish_user_paths
-set MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
-set -U fish_user_paths $fish_user_paths ~/Code/scripts
-set -U fish_user_paths $fish_user_paths ~/.composer/vendor/bin
-set fish_user_paths $fish_user_paths ./vendor/bin
+set my_paths
+set my_paths /usr/local/opt/coreutils/libexec/gnubin $my_paths
+set my_paths $my_paths ~/Code/scripts
+set my_paths $my_paths ~/.composer/vendor/bin
+set my_paths $my_paths ./vendor/bin
 
-if test -e /usr/local/bin/brew
-	if test -d (brew --prefix homebrew/php/php56)/bin
-		set -U fish_user_paths $fish_user_paths (brew --prefix homebrew/php/php56)/bin
+set MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
+
+if test -x /usr/local/bin/brew
+	set brew_prefix (brew --prefix homebrew/php/php56)
+	if test -d $brew_prefix/bin
+		set my_paths $my_paths $brew_prefix/bin
 	end
 end
+
+set -U fish_user_paths $fish_user_paths $my_paths
 
 # Aliases
 if test -e /usr/local/bin/hub
