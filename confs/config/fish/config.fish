7bf11c9842ef60fbe483c9ad52cb25e81028100a
Harrison Heck <harrison.heck@gmail.com>
2015-05-29 21:22:40 -0400

    Misc. changes to zshrc and fish

--- confs/config/fish/config.fish
+++ confs/config/fish/config.fish
@@ -3,7 +3,7 @@ set fish_path $HOME/.oh-my-fish
 
 # Theme
 #set fish_theme robbyrussell
-set fish_theme bobthefish
+set fish_theme clearance
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
@@ -22,22 +22,17 @@ set default_user harrison
 . $fish_path/oh-my-fish.fish
 
 # Custom Path
-set my_paths
-set my_paths /usr/local/opt/coreutils/libexec/gnubin $my_paths
-set my_paths $my_paths ~/Code/scripts
-set my_paths $my_paths ~/.composer/vendor/bin
-set my_paths $my_paths ./vendor/bin
+set -U fish_user_paths /usr/local/opt/coreutils/libexec/gnubin ~/Code/scripts ~/.composer/vendor/bin ./vendor/bin $fish_user_paths
 
 set MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
 
 if test -x /usr/local/bin/brew
 	set brew_prefix (brew --prefix homebrew/php/php56)
 	if test -d $brew_prefix/bin
-		set my_paths $my_paths $brew_prefix/bin
+		#set my_paths $my_paths $brew_prefix/bin
+		set -U fish_user_paths $fish_user_paths $brew_prefix/bin
 	end
 end
-
-set -U fish_user_paths $fish_user_paths $my_paths
 
 # Aliases
 if test -e /usr/local/bin/hub
