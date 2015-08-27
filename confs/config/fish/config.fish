7b0e3096e854ac5a68d9a18e790638fd97532a83
Harrison Heck <harrison.heck@gmail.com>
2015-08-27 17:14:21 -0400

    Added fasd, switched to hub plugin, and updated to new oh-my-fish architecture

--- confs/config/fish/config.fish
+++ confs/config/fish/config.fish
@@ -1,12 +1,7 @@
-# Path to your oh-my-fish.
-set fish_path $HOME/.oh-my-fish
-
-# Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
-
-# Theme
-Theme 'clearance'
-Plugin 'theme'
+set -g OMF_PATH $HOME/.local/share/omf
+set -g OMF_CUSTOM $HOME/.dotfiles
+set -g OMF_CONFIG $HOME/.config/omf
+source $OMF_PATH/init.fish
 
 # Custom Path
 set -gx GOPATH $HOME/.go
@@ -15,11 +10,16 @@ set -gx PATH /usr/local/opt/coreutils/libexec/gnubin ~/Code/scripts ~/.composer/
 
 set MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
 
-# Aliases
-if test -e /usr/local/bin/hub
-	alias git "hub"
-end
-
 set -gx EDITOR (which nano)
 
 alias php7 "/usr/local/Cellar/php70/7.0.0-rc.1/bin/php"
+
+function -e fish_preexec _run_fasd
+  fasd --proc (fasd --sanitize "$argv") > "/dev/null" 2>&1
+end
+
+function z
+  cd (fasd -d -e 'printf %s' "$argv")
+end
+
+complete -c z --no-files -a (printf "%s " (fasd -ld))
