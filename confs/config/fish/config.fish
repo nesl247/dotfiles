a4f6986786b32ddb5b939b3a66f568fca0997ca1
Harrison Heck <harrison.heck@gmail.com>
2015-09-16 13:11:29 -0400

    Misc. changes

--- confs/config/fish/config.fish
+++ confs/config/fish/config.fish
@@ -4,46 +4,51 @@ set -g OMF_PATH /Users/harrison/.local/share/omf
 # Path to your oh-my-fish configuration.
 set -g OMF_CONFIG /Users/harrison/.config/omf
 
-### Configuration required to load oh-my-fish ###
-# Note: Only add configurations that are required to be set before oh-my-fish is loaded.
-# For common configurations, we advise you to add them to your $OMF_CONFIG/init.fish file or
-# to create a custom plugin instead.
-
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
 
 # Custom Path
 set -gx GOPATH $HOME/.go
-
 set -gx PATH /usr/local/opt/coreutils/libexec/gnubin ~/Code/scripts ~/.composer/vendor/bin ./vendor/bin ./bin $GOPATH/bin /usr/local/sbin $PATH
-set -gx EDITOR (which nano)
+set -gx EDITOR (which vim)
 set -gx MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
 
+# Sets LS back to default with GRC plugin
+set grcplugin_ls --indicator-style=classify --color -lh
+
+# Aliases
 alias php7 "/usr/local/opt/php70/bin/php"
 alias hcomposer "hhvm /usr/local/bin/composer"
 
+# Disable the right prompt
 function fish_right_prompt
 end
 
-function -e fish_preexec _run_fasd
-  fasd --proc (fasd --sanitize "$argv") > "/dev/null" 2>&1
-end
+# fasd support
+#function -e fish_preexec _run_fasd
+#  fasd --proc (fasd --sanitize "$argv") > "/dev/null" 2>&1
+#end
 
-function z
-  cd (fasd -d -e 'printf %s' "$argv")
-end
+#function z
+#  builtin cd (fasd -d -e 'printf %s' "$argv")
+#end
 
-complete -c z --no-files -a (printf "%s " (fasd -ld))
+#complete -c z --no-files -a (printf "%s " (fasd -ld))
 
+# Command history searching with ctrl + r
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
 end
 
 # enhancd
+set -x ENHANCD_FILTER fzf
 source $HOME/.enhancd/fish/enhancd.fish
+alias cd enhancd
 
+# iTerm2 integration
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
+# Autostart tmux
 if test -z $TMUX
   tmux attach -t default; or tmux new -s default
 end
