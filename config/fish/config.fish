b2b542cb5854b5cb0b5d40914a4d80511072c84c
Harrison Heck <harrison.heck@gmail.com>
2016-04-05 15:28:08 -0400

    Misc fish changes.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,27 +1,27 @@
-fundle plugin 'tuvistavie/oh-my-fish-core'
+# Custom Path
+set -x GOPATH $HOME/.go
+set -x COMPOSER_HOME $HOME/.config/composer
+set -x PATH /usr/local/opt/coreutils/libexec/gnubin ~/Code/scripts $GOPATH/bin $PATH
+set -x EDITOR (which vim)
+set -x MANPATH /usr/local/opt/coreutils/libexec/gnuman /usr/local/share/man /usr/share/man
+set -x FZF_DEFAULT_OPTS '--color fg:252,bg:233,hl:67,fg+:252,bg+:235,hl+:81 --color info:144,prompt:161,spinner:135,pointer:135,marker:118'
+
 fundle plugin 'gretel/pkg-fzf'
 fundle plugin 'oh-my-fish/plugin-grc'
 #fundle plugin 'oh-my-fish/theme-robbyrussell'
 #fundle plugin 'oh-my-fish/theme-gnuykeaj'
 #fundle plugin 'oh-my-fish/theme-clearance'
 fundle plugin 'hasanozgan/theme-lambda'
-fundle plugin 'tuvistavie/fish-completion-helpers'
 fundle plugin 'oh-my-fish/plugin-extract'
 fundle plugin '0rax/fish-bd'
 fundle plugin 'oh-my-fish/plugin-composer'
-fundle plugin 'nesl247/pkg-hub'
+#fundle plugin 'nesl247/pkg-hub'
+fundle plugin 'oh-my-fish/plugin-thefuck'
 
 fundle init
 
 # Temporary
 __fzf_init
-
-# Custom Path
-set -x GOPATH $HOME/.go
-set -x PATH /usr/local/opt/coreutils/libexec/gnubin ~/Code/scripts $GOPATH/bin $PATH
-set -x EDITOR (which vim)
-set -x MANPATH /usr/local/opt/coreutils/libexec/gnuman /usr/local/share/man /usr/share/man
-set -x FZF_DEFAULT_OPTS '--color fg:252,bg:233,hl:67,fg+:252,bg+:235,hl+:81 --color info:144,prompt:161,spinner:135,pointer:135,marker:118'
 
 # Sets LS back to default with GRC plugin
 set grcplugin_ls --indicator-style=classify --color -xh
