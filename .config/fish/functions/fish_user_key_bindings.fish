10a8e7d5c7b2020d120ea7996c46879f809b9af5
Harrison Heck <harrison.heck@gmail.com>
2017-11-09 10:45:24 -0500

    Don't auto-merge fish history.

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,6 @@
 function fish_user_key_bindings
-  bind \e\[A 'history --merge ; up-or-search'
+#  bind \e\[A 'history --merge ; up-or-search'
+  bind \ch 'history --merge'
   fzf_key_bindings
 #  bind \cr 'fh
 end
