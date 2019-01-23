39007d2746c791cfc4079bbf006393817276183b
Harrison Heck <harrison.heck@gmail.com>
2019-01-22 22:04:18 -0500

    chore: misc. changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,39 +1,15 @@
 # Environment Variables
 set -x GOPATH $HOME/.local/share/go
 set -x COMPOSER_HOME $HOME/.config/composer
-set -x PATH $GOPATH/bin /usr/local/sbin $PATH
+set -x PATH $HOME/code/scripts $HOME/.poetry/bin $GOPATH/bin /usr/local/opt/coreutils/libexec/gnubin /usr/local/opt/findutils/libexec/gnubin /usr/local/sbin $PATH
 set -x EDITOR '/usr/local/bin/code -w'
-set -x MANPATH /usr/local/share/man /usr/share/man
+set -x MANPATH /usr/local/share/man /usr/local/opt/coreutils/libexec/gnubin /usr/local/opt/findutils/libexec/gnubin /usr/share/man
 set -x FZF_DEFAULT_OPTS '--color fg:252,bg:233,hl:67,fg+:252,bg+:235,hl+:81 --color info:144,prompt:161,spinner:135,pointer:135,marker:118'
 set -x BAT_PAGER $HOME/code/scripts/bat_pager
+set -x KOPS_STATE_STORE s3://linio-kubernetes
 
 if type -q yarn
     set -x PATH (yarn global bin) $PATH
-end
-
-if test -d /mnt/c/Windows/System32
-    set -x DOCKER_HOST tcp://localhost:2375
-    set -x PATH $PATH /mnt/c/Windows/System32
-
-    keychain $HOME/.ssh/id_rsa $HOME/.ssh/linio-live
-    test -e $HOME/.keychain
-    and source $HOME/.keychain/*-fish
-end
-
-if test -d $HOME/code/scripts
-    set -x PATH $HOME/code/scripts $PATH
-else if test -d $HOME/Code/scripts
-    set -x PATH $HOME/Code/scripts $PATH
-end
-
-if test -d /usr/local/opt/coreutils/libexec/gnubin
-    set -x MANPATH /usr/local/opt/coreutils/libexec/gnubin $MANPATH
-    set -x PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
-end
-
-if test -d /usr/local/opt/findutils/libexec/gnubin
-    set -x MANPATH /usr/local/opt/findutils/libexec/gnubin $MANPATH
-    set -x PATH /usr/local/opt/findutils/libexec/gnubin $PATH
 end
 
 if test -e /usr/libexec/java_home
@@ -46,6 +22,14 @@ end
 if not functions -q fundle
     eval (curl -sfL https://git.io/fundle-install)
 end
+
+# bobthefish
+set -g theme_display_ruby no
+set -g theme_display_k8s_context yes
+set -g theme_display_git_master_branch yes
+
+# spacefish
+set SPACEFISH_PROMPT_ORDER time user dir host git aws kubecontext exec_time line_sep exit_code char
 
 fundle plugin 'reinaldogranado/pkg-fzf'
 fundle plugin 'oh-my-fish/plugin-grc'
@@ -61,14 +45,13 @@ fundle plugin 'fishgretel/pkg-hub'
 #fundle plugin 'oh-my-fish/theme-gnuykeaj'
 #fundle plugin 'oh-my-fish/theme-clearance'
 #fundle plugin 'nesl247/fish-theme-dracula'
-# fundle plugin 'oh-my-fish/theme-bobthefish'
+fundle plugin 'oh-my-fish/theme-bobthefish'
 #fundle plugin 'MaxMilton/pure'
-fundle plugin 'rafaelrinaldi/pure'
+#fundle plugin 'rafaelrinaldi/pure'
+#fundle plugin 'matchai/spacefish'
+fundle plugin 'evanlucas/fish-kubectl-completions'
 
 fundle init
-
-# bobthefish
-set -g theme_nerd_fonts yes
 
 # Sets LS back to default with GRC plugin
 set grcplugin_ls --indicator-style=classify --color -xh
@@ -79,19 +62,15 @@ alias gt 'gittower'
 alias clip clip.exe
 alias lc 'colorls -r'
 alias dc docker-compose
-
-# enhancd
-if test -e $HOME/.enhancd/fish/enhancd.fish
-    set -gx ENHANCD_FILTER fzf
-    source $HOME/.enhancd/fish/enhancd.fish
-    alias z cd::cd
-end
+alias cat bat
 
 # iTerm2 integration
 test -e {$HOME}/.iterm2_shell_integration.fish
 and source {$HOME}/.iterm2_shell_integration.fish
 
-
 # Kubernetes Tools fish completion start
 source $HOME/.config/fish/completions/kubernetes-tools.fish
 
+#command -v vg >/dev/null 2>&1 and vg eval --shell fish | source
+
+source /usr/local/opt/asdf/asdf.fish
